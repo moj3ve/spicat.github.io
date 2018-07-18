2c66dd74bc904da58ed0609794796a1d913ca44a
SpicaT <spica.tweak@outlook.com>
2018-07-18 13:35:16 +0200

    Update.

--- import.sh
+++ import.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -f Packages*
-./dpkg-scanpackages -m ./debs /dev/null | gzip > Packages.gz;
-./dpkg-scanpackages -m ./debs /dev/null | bzip2 > Packages.bz2;
-./dpkg-scanpackages -m ./debs /dev/null > Packages;
+# Thanks PoomSmart!
+./dpkg-scanpackages -m ./debs /dev/null > Packages
+gzip -cf Packages > Packages.gz
+bzip2 -cf Packages > Packages.bz2
